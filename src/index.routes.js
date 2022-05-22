6e65892174a87882c6627510f29c3eb9bcb701e3
Mohamed El-khamisi <mohammd.alkhamisi@gmail.com>
2022-05-22 17:27:53 +0200

    session[B

--- src/index.routes.js
+++ src/index.routes.js
@@ -58,13 +58,14 @@ module.exports = async (app) => {
     session({
       name: 's_id',
       secret: TOKENKEY,
-      store: MongoStore.create({ clientPromise }),
-      resave: false,
-      saveUninitialized: true,
+      // store: MongoStore.create({ clientPromise }),
+      // resave: false,
+      // saveUninitialized: true,
       httpOnly: false,
       maxAge: 24 * 60 * 60 * 1000, //24 Hours OR Oneday
       sameSite: NODE_ENV == 'dev'? false : 'none',
       secure: NODE_ENV == 'dev' ? false : true,
+      
     })
   );
   const unless = function (paths, middleware) {
