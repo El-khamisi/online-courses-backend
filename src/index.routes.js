a428bf05dbfc241b852fe632bb802990c5336bb8
Mohamed El-khamisi <mohammd.alkhamisi@gmail.com>
2022-05-22 15:54:27 +0200

    session

--- src/index.routes.js
+++ src/index.routes.js
@@ -19,7 +19,7 @@ const role = require('./services/role/role.routes');
 const profile = require('./services/user/profile.routes');
 
 module.exports = async (app) => {
-  app.use(cookieParser());
+  // app.use(cookieParser());
   app.use(express.json());
   app.use(morgan('dev'));
 
@@ -86,7 +86,7 @@ module.exports = async (app) => {
   };
   app.use(unless(['/admin/course/*', '/admin/user/*', '/myprofile'], multer().none()));
 
-  
+
   //Routers
   app.use(login);
   app.use(dashboard);
