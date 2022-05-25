9792359d57b94f5d3706463805567481cff3a260
Mohamed El-khamisi <mohammd.alkhamisi@gmail.com>
2022-05-25 14:37:50 +0200

    Bearer

--- src/middelwares/authN.js
+++ src/middelwares/authN.js
@@ -4,7 +4,7 @@ const { failedRes } = require('../utils/response');
 
 exports.authN = (req, res, next) => {
   try {
-    if (!req.get('Authorization') || !req.cookies.authorization) {
+    if (!req.get('Authorization') && !req.cookies.authorization) {
       throw new Error('Login first');
     }
     const token = req.get('Authorization').split(' ')[1]|| req.cookies.authorization; 
