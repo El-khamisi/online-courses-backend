51db91e5544092307a74101f645ff1ab89748d04
Mohamed El-khamisi <mohammd.alkhamisi@gmail.com>
2022-05-30 15:48:38 +0200

    quizzes

--- src/services/quiz/quiz.controllers.js
+++ src/services/quiz/quiz.controllers.js
@@ -125,14 +125,18 @@ exports.submitQuiz = async (req, res) => {
     const usr = await User.findById(user_id).exec();
     
     let flag = false;
-    for(const obj of usr.quizzes){
-      if(obj._id == quiz_id){
-        obj.date =  (()=>new Date())();
-        obj.score = response.total;
+    for(let i=0; i<usr.quizzes.length; i++) {
+      if(usr.quizzes[i]._id == quiz_id) {
+        console.log(usr.quizzes[i]._id);
         flag = true;
+        usr.quizzes[i].date = new Date();
+        usr.quizzes[i].score = response.total;
+        console.log(usr.quizzes[i]._id);
+
         break;
       }
     }
+    
 
     if(flag == false){
       usr.quizzes.push({
