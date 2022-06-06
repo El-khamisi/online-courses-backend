51bfd89f46ad8b0d209aac0e4474e785d78f42d1
Mohamed El-khamisi <mohammd.alkhamisi@gmail.com>
2022-06-06 12:01:23 +0200

    updupdate profile

--- src/services/login/login.routes.js
+++ src/services/login/login.routes.js
@@ -4,7 +4,7 @@ const {authN} = require('../../middelwares/authN');
 
 router.post('/login', logUser);
 router.post('/signup', regUser);
-router.post('/reset-password', authN, resetPassword);
+router.put('/reset-password', authN, resetPassword);
 router.post('/logout', logout);
 
 module.exports = router;
